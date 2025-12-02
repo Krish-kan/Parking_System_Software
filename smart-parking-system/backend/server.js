0721fa6b1a47f6fd9447b944a6b636dc4113ec1d
Krish-kan <142814937+Krish-kan@users.noreply.github.com>
2025-12-02 15:57:08 +0530

    Change server port from 3000 to 5000

--- smart-parking-system/backend/server.js
+++ smart-parking-system/backend/server.js
@@ -30,5 +30,5 @@ app.use('/api/reservations', reservationRoutes);
 app.use('/api/payments', paymentsRoutes);
 app.use('/api/qr', qrRoutes);
 
-const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => console.log(`Listening on ${PORT}`));
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
